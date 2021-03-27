70cbf541f352139ee43cf017655656801ba2125c
oceanumeric <numerical.ocean@gmail.com>
2021-03-27 21:48:50 +0800

    [add] new files

---
+++ tests/app_test.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from project.app import app
+
+
+def test_index():
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
+
+
+def test_database():
+    assert Path("flask.db").is_file() 
